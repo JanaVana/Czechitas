a581b1954a93e5cf7801eca946ad1af7cc6a5ba3
JanaVana <vanova.yana@gmail.com>
2022-02-22 17:48:26 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,6 +1,14 @@
 console.log('ahoj svete');
 let jirik;
 jirik = 57.544;
+let jmeno = 'Marian';
 
+let prsi= true;
+
+//prsi - true = pravda
+
+//nesnezi
+
+let snezi = false;
 
 console.log(jirik);
